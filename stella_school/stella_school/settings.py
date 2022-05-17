56fdc647d69dd54603c103dd925b0507e68ae58c
Ranjith R <72377429+Ranjithr-007@users.noreply.github.com>
2022-05-17 13:47:41 +0530

    Update settings.py

--- stella_school/stella_school/settings.py
+++ stella_school/stella_school/settings.py
@@ -135,13 +135,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'stellamarishanga@gmail.com'
+EMAIL_HOST_USER = 'stellamarisahmednagar@gmail.com'
 EMAIL_HOST_PASSWORD = 'hangastellamaris'
-ADMIN_RECEIVE_EMAIL = 'stellamarisahmednagar@gmail.com'
+ADMIN_RECEIVE_EMAIL = 'stellamarishanga@gmail.com'
 DEFAULT_FROM_EMAIL = 'stellamarisahmednagar@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
 
 
 
