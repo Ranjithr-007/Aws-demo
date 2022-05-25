1ab6f0b973b49031003e27cdad09cf8e0cb90776
Ranjith R <72377429+Ranjithr-007@users.noreply.github.com>
2022-05-25 21:15:18 +0530

    Update urls.py

--- stella_school/src/urls.py
+++ stella_school/src/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('', homepage, name='index'),
     path('academic-calendar/', calendar, name ='calendar'),
-    path('school_fee/',school_fee, name='school_fee')
+    path('school_fee/',school_fee, name='school_fee'),
 ]
 
