3a81e3262cfc55cc4f6342a5bf50b8aaa465f404
Ranjith <ranjithranjukpm333@gmail.com>
2022-05-25 19:03:32 +0530

    final commit

--- stella_school/src/urls.py
+++ stella_school/src/urls.py
@@ -4,9 +4,6 @@ from .views import *
 urlpatterns = [
     path('', homepage, name='index'),
     path('academic-calendar/', calendar, name ='calendar'),
-<<<<<<< HEAD
     path('school_fee/',school_fee, name='school_fee')
 ]
-=======
-]
->>>>>>> cbc8ab5d28d437ac5f2355fa3abcff7f9149c16f
+
