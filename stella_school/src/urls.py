57fd67ab6587d7b818a9d56037a165f49e0c6141
Ranjith <ranjithranjukpm333@gmail.com>
2022-06-13 23:34:54 +0530

    final commit

--- stella_school/src/urls.py
+++ stella_school/src/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('noc/', noc, name='noc'),
     path('rte/', rte, name='rte'),
     path('water-sanitation-certificate/', water_sanitation_certificate, name='water_sanitation_certificate'),
-    path('school_fee/',school_fee, name='school_fee'),
     path('society-registration-certificate', society_registration_certificate, name='society_registration_certificate'),
     path('pta/', pta, name='pta'),
     path('smc/', school_managing_commitee, name='school_managing_commitee'),
